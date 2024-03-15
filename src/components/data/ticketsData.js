fea882c8f62506b41d50a11ab6c9e2aa0e887e2a
ikromshi <numanovikrom454@gmail.com>
2024-03-15 13:47:46 -0400

    initial set-up; and bars complete

---
+++ src/components/data/ticketsData.js
@@ -0,0 +1,10 @@
+// src/data/ticketsData.js
+const ticketsData = [
+    { name: "Hannah", tickets: 5 },
+    { name: "Ikrom", tickets: 8 },
+    { name: "Josh", tickets: 2 },
+    { name: "Jack", tickets: 4 },
+    // more data
+  ];
+  export default ticketsData;
+  
